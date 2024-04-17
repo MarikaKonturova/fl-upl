bb64fd9775f386a53dad50fde4451b773d0225ca
MarikaKonturova <mashakonturova@gmail.com>
2024-04-17 08:44:34 +0300

    fix: title in dialogs

--- app/components/DialogRabota.tsx
+++ app/components/DialogRabota.tsx
@@ -69,7 +69,7 @@ const DialogRabota = ({
         <Dialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
         <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[600px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
           <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
-            Изменить данные о работе
+            {type === "edit" ? "Изменить данные о работе" : "Добавить работу"}
           </Dialog.Title>
           <fieldset className="mb-[15px] flex items-center gap-5">
             <label
